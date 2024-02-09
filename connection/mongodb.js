69c6f7f5785ed6a96181725353189f6f8d687784
Ayush Kumar <ayushkumar0352@gmail.com>
2024-02-10 05:09:48 +0530

    Minor fixes

--- connection/mongodb.js
+++ connection/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 exports.connectDb = async () => {
     try {
-        const data = await mongoose.connect("mongodb+srv://aksapple1610:DLTest@dl.qxgye6d.mongodb.net/Learners");
+        const data = await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to mongoDb");
     } catch (error) {
         console.log("An error occoured in connecting mongoDb", error);
